dfc85067c43912baff0604fb9e095303cdb5d0e3
Mark Cedrick Tacorda <158182874+markyuxir23@users.noreply.github.com>
2024-06-11 16:42:18 +0800

    restriction done (v2)

--- NMA/scripts/DestLocations.gd
+++ NMA/scripts/DestLocations.gd
@@ -6,9 +6,16 @@ extends VBoxContainer
 @onready var _4fLocations = preload("res://NMA/scenes/NMA scenes/4f_locations.tscn").instantiate().get_children()
 signal buttonPressed(buttonName)
 
+
+var restrictDB = Firebase.Database.get_database_reference("restrictedData/Galleries/")
+var restrictData: Dictionary
+
 var include2F = true
 var include3F = true
 var include4F = true
+
+var currentGalleries
+var currentTag
 
 @export var _2F_Tag: Texture
 @export var _3F_Tag: Texture
@@ -21,22 +28,45 @@ func _ready():
 		_on_option_button_item_selected(1)
 	if origin.is_in_group("4F"):
 		_on_option_button_item_selected(2)
+		
+	restrictDB.update("dummy", {0:0})
+	
+	restrictDB.new_data_update.connect(updateData)
+	restrictDB.patch_data_update.connect(updateData)
+	restrictDB.delete_data_update.connect(updateData)
+
+func updateData(document):
+	if get_children().size() != 0 or null:
+		for i in get_children():
+			if i.name != "Button":
+				remove_child(i)
+
+	restrictData = restrictDB.get_data()
+	for loc in currentGalleries:
+		if restrictData.keys().has(loc.name):
+			loc.name = loc.name + "(RESTRICTED)"
+	constructButtons(currentGalleries, currentTag)
 
 func constructButtons(locations, tag):
 	for loc in locations:
-		var button = %Button.duplicate()
+		var button = get_child(0).duplicate()
 		add_child(button)
 		button.show()
 		button.text = loc.name
 		var itemName = loc.name
 		button.icon = tag
-		button.pressed.connect(self.emitName.bind(itemName))
+		if loc.name.contains("(RESTRICTED)"):
+			continue
+		else:
+			button.pressed.connect(self.emitName.bind(itemName))
 		
 func emitName(buttonName):
 	emit_signal("buttonPressed", buttonName)
 
 func _on_option_button_item_selected(index):
 	if index == 0:
+		currentGalleries = _2fLocations
+		currentTag = _2F_Tag
 		for child in get_children():
 			if child == %Button:
 				continue
@@ -44,6 +74,8 @@ func _on_option_button_item_selected(index):
 				child.queue_free()
 		constructButtons(_2fLocations, _2F_Tag)
 	if index == 1:
+		currentGalleries = _3fLocations
+		currentTag = _3F_Tag
 		for child in get_children():
 			if child == %Button:
 				continue
@@ -51,10 +83,11 @@ func _on_option_button_item_selected(index):
 				child.queue_free()
 		constructButtons(_3fLocations, _3F_Tag)
 	if index == 2:
+		currentGalleries = _4fLocations
+		currentTag = _4F_Tag
 		for child in get_children():
 			if child == %Button:
 				continue
 			else:
 				child.queue_free()
 		constructButtons(_4fLocations, _4F_Tag)
-
